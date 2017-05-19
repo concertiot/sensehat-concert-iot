e52113bb00cba7e7d0be50ee35dd4451c57483a1
concertiot <david.bacon@aepona.com>
2017-05-19 12:42:51 +0100

    adding in account url

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ deviceTypesAPI = '/api/1/device-types'
 authUrl = 'https://sso.' + concertURL + authTokenAPI
 devicesUrl='https://api.' + concertURL + devicesAPI
 deviceTypesUrl = 'https://api.' + concertURL + deviceTypesAPI
-
+accountUrl = 'https://api.' + concertURL + '/api/1/accounts'
 
 #Device Details
 deviceTypeId='6c21d5c9-34fa-46dd-8155-c8461cb2e6b5'
